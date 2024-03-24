eb5e49142d82fa5733351c9049a1ebec22df2063
Akshay Choudhry <mail@akshayc.dev>
2024-03-24 14:26:29 +0100

    initialize yarn project

---
+++ website/.prettierrc.js
@@ -0,0 +1,66 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: ['*.gql', '*.graphql'],
+      options: {
+        parser: 'graphql',
+      },
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      options: {
+        parser: 'babel',
+      },
+    },
+    {
+      files: '*.json',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 60,
+        useTabs: false,
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '*.mdx',
+      options: {
+        parser: 'mdx',
+        printWidth: 60,
+        useTabs: false,
+        tabWidth: 2,
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.stories.ts', '*.stories.tsx'],
+      options: {
+        parser: 'typescript',
+        useTabs: false,
+      },
+    },
+    {
+      files: ['*.yml', '*.yaml'],
+      options: {
+        parser: 'yaml',
+        useTabs: false,
+      },
+    },
+  ],
+};
