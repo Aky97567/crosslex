9a20cddd74738ad8ece4223bc96cecac61486a0b
Akshay Choudhry <mail@akshayc.dev>
2024-06-08 22:35:11 +0200

    upgrade storybook to `7.6.10`

--- website/sites/storybook/.storybook/main.js
+++ website/sites/storybook/.storybook/main.js
@@ -1,6 +1,8 @@
+import { dirname, join } from "path";
 /** @type {import('@storybook/types').StorybookConfig} */
 module.exports = {
   stories: ['../../../frontend/*/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+
   addons: [
     {
       name: '@storybook/addon-styling',
@@ -10,14 +12,19 @@ module.exports = {
         },
       },
     },
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: '@storybook/nextjs',
+
+  framework: getAbsolutePath("@storybook/nextjs"),
+
   core: {
     disableTelemetry: true,
   },
+
+  // TODO: Add static directory public
+  // staticDirs: ['../public'],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.tsx?$/,
@@ -53,6 +60,12 @@ module.exports = {
 
     return config;
   },
-  // TODO: Add static directory public
-  // staticDirs: ['../public'],
+
+  docs: {
+    autodocs: true
+  }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
