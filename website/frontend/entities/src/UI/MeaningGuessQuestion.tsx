e45d4a7035af1596d0a189d20e08fdd41460f353
Akshay Choudhry <mail@akshayc.dev>
2024-08-03 23:25:04 +0200

    improve question UI

--- website/frontend/entities/src/UI/MeaningGuessQuestion.tsx
+++ website/frontend/entities/src/UI/MeaningGuessQuestion.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { MeaningBestGuessQuestion } from '@whitelotus/common-crosslex-view';
 import { Card } from '@whitelotus/front-shared';
 
@@ -7,14 +8,51 @@ type MeaningGuessQuestionProps = {
 
 export const MeaningGuessQuestion: React.FC<MeaningGuessQuestionProps> = ({
   meaningBestGuessQuestion,
-}) =>
-  meaningBestGuessQuestion ? (
+}) => {
+  const [optionStates, setOptionStates] = useState<
+    Array<{ isClicked: boolean; isCorrect: boolean }>
+  >(
+    meaningBestGuessQuestion.options.map(() => ({
+      isClicked: false,
+      isCorrect: false,
+    })),
+  );
+
+  const handleOptionClick = (index: number) => {
+    const newOptionStates = optionStates.map((state, i) =>
+      i === index
+        ? {
+            isClicked: true,
+            isCorrect: meaningBestGuessQuestion.options[index].isCorrect,
+          }
+        : state,
+    );
+    setOptionStates(newOptionStates);
+  };
+
+  return meaningBestGuessQuestion ? (
     <Card>
       <div className="mb-6">
         <h2 className="text-2xl font-semibold mb-2">
           Meaning Best Guess Question
         </h2>
         <p className="text-lg">{meaningBestGuessQuestion.question}</p>
+        {meaningBestGuessQuestion.options.map((option, index) => (
+          <div
+            key={index}
+            className={`text-md cursor-pointer border-2 rounded-lg px-40 py-10 mb-10 transition-colors duration-300 ${
+              optionStates[index].isClicked
+                ? optionStates[index].isCorrect
+                  ? 'border-color1 text-color1 bg-color1-light'
+                  : 'border-color3 text-color3 bg-color3-light'
+                : 'border-color7 text-black hover:bg-color7-light'
+            }`}
+            onClick={() => handleOptionClick(index)}
+          >
+            {option.text}
+          </div>
+        ))}
       </div>
     </Card>
   ) : null;
+};
