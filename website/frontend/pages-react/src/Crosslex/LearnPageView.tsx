37062689cc38cc8bd6794fb7d3967169c0200184
Akshay Choudhry <mail@akshayc.dev>
2026-05-03 18:08:13 +0200

    refactor: rename front-pages to front-pages-next, add front-widgets and front-pages-react

---
+++ website/frontend/pages-react/src/Crosslex/LearnPageView.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { LearnPage } from '@whitelotus/front-widgets';
+import { HasContent } from '@whitelotus/common-crosslex-view';
+
+const LearnPageView: React.FC<HasContent> = (props) => <LearnPage {...props} />;
+
+export { LearnPageView };
