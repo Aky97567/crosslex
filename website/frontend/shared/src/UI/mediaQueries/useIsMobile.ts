82719f3b93757324adf18e3cbc686fb3b94353a9
Akshay Choudhry <mail@akshayc.dev>
2026-05-06 10:44:08 +0200

    fix/NO-TICKET export SelectableCard from UI index; default useIsMobile to true for mobile-first

--- website/frontend/shared/src/UI/mediaQueries/useIsMobile.ts
+++ website/frontend/shared/src/UI/mediaQueries/useIsMobile.ts
@@ -3,7 +3,7 @@ import { getBreakpoint } from './mediaQueries';
 
 // Check for mobile devices that have virtual keyboard
 export const useIsMobile = () => {
-  const isMobile = useMedia(`(max-width: ${getBreakpoint('md')}px)`, false);
+  const isMobile = useMedia(`(max-width: ${getBreakpoint('md')}px)`, true);
 
   return isMobile;
 };
