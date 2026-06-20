0ef2b36e71ad95de4d944924f08164b0b8e65d9a
Akshay Choudhry <mail@akshayc.dev>
2026-06-20 13:10:12 +0200

    fix SessionCardRenderer story: wrap with CrosslexStorageProvider

--- website/frontend/pages-react/src/Crosslex/SessionRunner/SessionCardRenderer/SessionCardRenderer.stories.tsx
+++ website/frontend/pages-react/src/Crosslex/SessionRunner/SessionCardRenderer/SessionCardRenderer.stories.tsx
@@ -1,7 +1,23 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj, Decorator } from '@storybook/react';
 import { sampleLearnPageContentList, A2Words } from '@whitelotus/mock-test';
-import { generateExerciseDataSafe } from '@whitelotus/front-features';
+import {
+  generateExerciseDataSafe,
+  CrosslexStorageProvider,
+  createMemoryAdapter,
+} from '@whitelotus/front-features';
 import { SessionCardRenderer } from './SessionCardRenderer';
+
+const emptyAdapter = createMemoryAdapter({
+  exerciseLog: [],
+  streak: null,
+  activeLevel: 'b1',
+});
+
+const withStorage: Decorator = Story => (
+  <CrosslexStorageProvider adapter={emptyAdapter}>
+    <Story />
+  </CrosslexStorageProvider>
+);
 
 const kuendigung = sampleLearnPageContentList['kuendigung'].content;
 const konto = sampleLearnPageContentList['konto'].content;
@@ -9,6 +25,7 @@ const konto = sampleLearnPageContentList['konto'].content;
 export default {
   title: 'Widgets/SessionRunner/SessionCardRenderer',
   component: SessionCardRenderer,
+  decorators: [withStorage],
   parameters: { layout: 'padded' },
   args: {
     reviewContent: null,
