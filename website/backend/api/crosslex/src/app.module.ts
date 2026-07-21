63a2743fc18c1a0b5aa5e4dbd3d21452e1c5ad0d
Akshay Choudhry <mail@akshayc.dev>
2026-07-21 19:33:35 +0200

    feat: NestJS GraphQL API skeleton (backend/api/crosslex workspace) (#80)

---
+++ website/backend/api/crosslex/src/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'node:path';
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      sortSchema: true,
+    }),
+    HealthModule,
+  ],
+})
+export class AppModule {}
